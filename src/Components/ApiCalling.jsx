99190dce0e2171a515c6e25ccf6001139131629e
Priyansh Rana <154973761+Priyansh159@users.noreply.github.com>
2025-05-05 12:02:35 +0530

    Update ApiCalling.jsx

--- src/Components/ApiCalling.jsx
+++ src/Components/ApiCalling.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const ApiCalling = () => {
     
     let [allRestaurantArr, setAllRestaurantArr] = useState([]);
-    const API = "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    const API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     useEffect(()=>{
         async function calling(){
             let resp = await axios.get(API)
